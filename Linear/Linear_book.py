ae95f97f720e56f7e0b272cedd68639e2c493c04
VIVI1310 <violet280903@gmail.com>
2023-06-25 14:33:25 +0700

    Datasheet imported

--- Linear/Linear_book.py
+++ Linear/Linear_book.py
@@ -1,54 +1,46 @@
-import math
-from math import *
-import numpy
-from numpy import *
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 18 22:06:34 2019
 
-global X1, X2, Y, W, component, default_rate
-X1 = [[0], [0], [1], [1]]
-X2 = [[0], [1], [0], [1]]
-Y = [[0], [0], [0], [1]]
+@author: DELL
+"""
 
-X = ones((4, 1), dtype=int)
-X = append(X, X1, axis=1)
-X = append(X, X2, axis=1)  # (4, 3)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#numOfPoint = 30
+#noise = np.random.normal(0,1,numOfPoint).reshape(-1,1)
+#x = np.linspace(30, 100, numOfPoint).reshape(-1,1)
+#N = x.shape[0]
+#y = 15*x + 8 + 20*noise
+#plt.scatter(x, y)
 
-default_rate = [[0.005], [0.005], [0.005]]
+data = pd.read_csv('data_linear.csv').values
+N = data.shape[0]
+x = data[:, 0].reshape(-1, 1)
+y = data[:, 1].reshape(-1, 1)
+plt.scatter(x, y)
+plt.xlabel('mét vuông')
+plt.ylabel('giá')
 
-n = 4  # number of exampples
-component = int(3)  # số tham số: bias, w1, w2
-W = zeros((3, 1))
+x = np.hstack((np.ones((N, 1)), x))
 
+w = np.array([0.,1.]).reshape(-1,1)
 
-def y_prediction():
-    y = X @ W   # (4, 1)
-    return y
+numOfIteration = 100
+cost = np.zeros((numOfIteration,1))
+learning_rate = 0.000001
+for i in range(1, numOfIteration):
+    r = np.dot(x, w) - y
+    cost[i] = 0.5*np.sum(r*r)
+    w[0] -= learning_rate*np.sum(r)
+    # correct the shape dimension
+    w[1] -= learning_rate*np.sum(np.multiply(r, x[:,1].reshape(-1,1)))
+    print(cost[i])
+predict = np.dot(x, w)
+plt.plot((x[0][1], x[N-1][1]),(predict[0], predict[N-1]), 'r')
+plt.show()
 
-
-def derived_W():
-    d = X.transpose() @ (y_prediction() - Y) * 1 / n * 2
-    return d
-
-def predict():
-    y = X @ W
-    for _ in range (size(y)):
-        if abs(1 - y[_]) < abs(y[_]):
-            y[_] = int(1)
-            print(y[_])
-        else:
-            y[_] = int(0)
-            print(Y[_])
-    return y
-def gradient_descent(learn_rate, iteration=100):
-    global W
-    for _ in range(iteration):
-        W -= learn_rate * derived_W()
-    return predict()
-
-def input():
-    global learn_rate, iteration
-    learn_rate = zeros(component, dtype, int)
-    learn_rate = float(input("Input learning rate (bias first): ").split())
-    iteration = int(input("Input iteration (integer) :"))
-
-
-gradient_descent(default_rate, 10000)
+x1 = 50
+y1 = w[0] + w[1] * 50
+print('Giá nhà cho 50m^2 là : ', y1)
