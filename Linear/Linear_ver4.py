611ccc5047d13830234dfb15d7b40018cb065124
VIVI1310 <violet280903@gmail.com>
2023-07-08 13:35:54 +0700

    no comment

--- Linear/Linear_ver4.py
+++ Linear/Linear_ver4.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # IMPORT DATA
-data = pd.read_csv('data_linear.csv').values
-N, d = data.shape       # N = row   d = column
-x = data[:, 0].reshape(-1, 1)
-x_train = append(x, ones((N, 1)), axis = 1)
-y_train = data[:, 1].reshape(-1, 1)
+data = pd.read_csv('data_linear.csv').values    # nhập hêết dữ liệu từ data sheet
+N, d = data.shape       # N = row   d = column  # lấy số cột và dòng
+x = data[:, 0].reshape(-1, 1)                   # tachs dữ liệu của x -> ma trận cột [N, 1]
+x_train = append(x, ones((N, 1)), axis = 1)     # thêm 1 côt và fill = 1
+y_train = data[:, 1].reshape(-1, 1)             # tách dữ liệu của y
 
 # TRAIN MODEL
 w = zeros((d, 1))
 def linear_predict(w, x_):
-    return x_ @ w
+    return x_ @ w       #predict y
 def gradient_descent(w, x_train, y_train, lr, iteration):
     for _ in range(iteration):
-        w -= multiply(lr / N, x_train.transpose() @ (linear_predict(w, x_train) - y_train) )
+        w -= multiply(lr / N, x_train.transpose() @ (linear_predict(w, x_train) - y_train) ) 
     print(w)
 
 # MODEL
